db02b9f88cc39b7acf827252fcfbc5936aa59ff1
liquid <liquid>
2025-02-04 00:04:09 -0800

    first commit

---
+++ vue/intro/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  const value = "???"
+  const othervalue = "no"
+  const color = "color: red;"
+  const colors = ["purple", "brown", "red", "orange", "yellow"]
+  const yesorno = false
+</script>
+
+<template>
+  <h1>goodbye world {{ value }}</h1>
+  <h1 v-bind:style="color">{{ othervalue.toUpperCase() }}</h1>
+  <h2 :style="`color: ${ colors[3] }`">olaaaaaaaaaa</h2>
+  <h1>{{ colors }}</h1>
+  <h2>{{ yesorno ? "sisi" : "no, feo" }}</h2>
+
+  
+  <h2 v-if="yesorno">u black my friend</h2>
+  <h2 v-else="yesorno">u not black my friend thank god</h2>
+
+</template>
+
+<style>
+  h1{
+    color: #202020;
+  }
+</style>
