dcce8c813f635ed558b5486df9afa6d14039df13
liquid <liquid>
2025-02-04 18:13:49 -0800

    v-for structure

--- vue/intro/src/App.vue
+++ vue/intro/src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
   const value = "???"
   const othervalue = "no"
   const color = "color: red;"
@@ -12,7 +12,6 @@
   <h2 :style="`color: ${ colors[3] }`">olaaaaaaaaaa</h2>
   <h1>{{ colors }}</h1>
   <h2>{{ yesorno ? "sisi" : "no, feo" }}</h2>
-
   
   <h2 v-if="yesorno">u black my friend</h2>
   <h2 v-else="yesorno">u not black my friend thank god</h2>
@@ -23,4 +22,48 @@
   h1{
     color: #202020;
   }
-</style>
+</style> -->
+
+<script setup>
+  const blocks = [
+    {
+      id: 1,
+      name: "Dirt",
+      recipes: [
+        {id: 1, title: "coarse dirt"},
+      ]
+    },
+    {
+      id: 2,
+      name: "Cobblestone",
+      recipes: [
+        {id: 1, title: "stone sword"},
+        {id: 2, title: "stone pickaxe"},
+        {id: 3, title: "furnance"},
+      ] 
+    },
+    {
+      id: 2,
+      name: "Diamondo",
+      recipes: [
+        {id: 1, title: "jukebox"},
+        {id: 2, title: "enchanting table"},
+        {id: 3, title: "diamond axe"},
+      ] 
+    }
+  ]
+</script>
+
+<template>
+  <div>
+    <h2>List of items used in minecraft recipes</h2>
+    <ul>
+      <template v-for="(block, index) in blocks":key="block.id">
+        <h2>{{ block.name }}</h2>
+        <template v-for="(recipe, index) in block.recipes":key="recipe.id">
+          <li>{{ recipe.id }} {{ recipe.title }}</li>
+        </template>
+      </template>
+    </ul>
+  </div>
+</template>
