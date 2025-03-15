147d181fea7c5f58eae3e53dfff1c9bc7a9281c7
liquid <liquid>
2025-03-14 18:41:37 -0700

    pagination component

---
+++ vue/components/src/components/PaginacionPost.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+defineProps({
+  start: Number,
+  end: Number,
+  disableNext: Boolean,
+  disableBack: Boolean,
+})
+
+const emit = defineEmits('back', 'next')
+
+</script>
+
+<template>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-outlined" @click="emit('back')" :disabled="disableBack">Back {{ start }}</button>
+    <button type="button" class="btn btn-outlined" @click="emit('next')" :disabled="disableNext">Next {{ end }}</button>
+  </div>
+</template>
